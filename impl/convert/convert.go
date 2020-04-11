15b533ad00538c83651f41a859860c94dc433ec3
Srini <s@srin.me>
2020-04-11 05:57:17 -0400

    show the desired lines on the terminal

--- impl/convert/convert.go
+++ impl/convert/convert.go
@@ -1,14 +1,15 @@
 package convert
 
 import (
-	"io"
+	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func convertFragment(rdr io.Reader, outbase string, idx int, frag string) {
+func convertFragment(inp *os.File, outbase string, idx int, frag string) {
 	linespec := strings.Split(frag, ":")
 	if len(linespec) != 2 {
 		log.Printf("Invalid fragment spec %s", frag)
@@ -25,6 +26,33 @@ func convertFragment(rdr io.Reader, outbase string, idx int, frag string) {
 	}
 
 	log.Printf("Will create %s.%d.png with lines %d to %d", outbase, idx, linefrom, linefrom+linecount-1)
+
+	inp.Seek(0, 0)
+	lineno := 0
+	rdr := bufio.NewReader(inp)
+	var line string
+	//var err error
+
+	for {
+		line, err = rdr.ReadString('\n')
+		if err != nil {
+			log.Printf("End of File before line %d", linefrom)
+			return
+		}
+		lineno = lineno + 1
+		if int64(lineno) >= linefrom {
+			break
+		}
+	}
+	for lc := 0; int64(lc) < linecount; lc = lc + 1 {
+		fmt.Printf("%04d | %s", linefrom+int64(lc), line)
+		line, err = rdr.ReadString('\n')
+		if err != nil {
+			log.Printf("End of file before reading %d lines", linecount)
+			return
+		}
+
+	}
 }
 
 func Convert(inp string, outbase string, frags []string) {
