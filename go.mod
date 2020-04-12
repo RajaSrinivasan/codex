8b7e96d4656c73d1a58019f3699379b4bdf7b177
Srini <s@srin.me>
2020-04-11 20:16:44 -0400

    ability to create multiple graphics files

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/spf13/cobra v0.0.7
 	github.com/spf13/viper v1.6.3
 	gitlab.com/RajaSrinivasan/exec v0.0.0-20200409184331-70ccc2e4d660 // indirect
+	gonum.org/v1/plot v0.7.0
 )
--- impl/convert/convert.go
+++ impl/convert/convert.go
@@ -3,11 +3,43 @@ package convert
 import (
 	"bufio"
 	"fmt"
+	"image/color"
 	"log"
 	"os"
 	"strconv"
 	"strings"
+
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 )
+
+var length vg.Length
+var width vg.Length
+var numMargin vg.Length
+var txtStyle draw.TextStyle
+var lineHeight vg.Length
+var leftMargin vg.Length
+var botMargin vg.Length
+
+func init() {
+	width, _ = vg.ParseLength("6.5in")
+	numMargin, _ = vg.ParseLength("0.75in")
+	txtStyle.Color = color.Black
+	txtStyle.Font, _ = vg.MakeFont("Helvetica", 12.0)
+	lineHeight = vg.Length(12.0)
+	leftMargin = vg.Length(6.0)
+	botMargin = vg.Length(6.0)
+}
+
+func paintLine(canvas draw.Canvas, idx int, line string, fromline int64, numlines int64) {
+	linestripped := strings.Replace(line, "\t", "    ", -1)
+	linenostr := fmt.Sprintf("%04d | %s", idx+int(fromline), linestripped)
+
+	lineidx := numlines - int64(idx) - 1
+	pt1 := vg.Point{X: 0.0 + leftMargin, Y: vg.Length(lineidx)*lineHeight + botMargin}
+	canvas.FillText(txtStyle, pt1, linenostr)
+	//fmt.Printf("%s\n", linenostr)
+}
 
 func convertFragment(inp *os.File, outbase string, idx int, frag string) {
 	linespec := strings.Split(frag, ":")
@@ -27,11 +59,14 @@ func convertFragment(inp *os.File, outbase string, idx int, frag string) {
 
 	log.Printf("Will create %s.%d.png with lines %d to %d", outbase, idx, linefrom, linefrom+linecount-1)
 
+	length = lineHeight * vg.Length(linecount)
+	pngcanvas, _ := draw.NewFormattedCanvas(width+2.0*leftMargin, length+2*botMargin, "png")
+	canvas := draw.NewCanvas(pngcanvas, width, length)
+
 	inp.Seek(0, 0)
 	lineno := 0
 	rdr := bufio.NewReader(inp)
 	var line string
-	//var err error
 
 	for {
 		line, err = rdr.ReadString('\n')
@@ -44,15 +79,20 @@ func convertFragment(inp *os.File, outbase string, idx int, frag string) {
 			break
 		}
 	}
-	for lc := 0; int64(lc) < linecount; lc = lc + 1 {
-		fmt.Printf("%04d | %s", linefrom+int64(lc), line)
+
+	paintLine(canvas, 0, line, linefrom, linecount)
+	for lc := 1; int64(lc) < linecount; lc = lc + 1 {
 		line, err = rdr.ReadString('\n')
 		if err != nil {
 			log.Printf("End of file before reading %d lines", linecount)
 			return
 		}
-
+		paintLine(canvas, lc, line, linefrom, linecount)
 	}
+	ofilename := fmt.Sprintf("%s.%d.png", outbase, idx)
+	ofile, _ := os.Create(ofilename)
+	pngcanvas.WriteTo(ofile)
+	ofile.Close()
 }
 
 func Convert(inp string, outbase string, frags []string) {
